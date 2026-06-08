a2c3ec87c943147050d537ca1852ffc13830f51d
Kevin Cabanilla <43958285+kevincabanilla@users.noreply.github.com>
2026-06-08 18:08:19 +0800

    update configs

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath } from "node:url";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,4 +11,16 @@ export default defineConfig({
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss(),
   ],
+  server: {
+    port: 8000,
+    open: false,
+  },
+  base: "/portfolio",
+  resolve: {
+    alias: {
+      // Always update the paths in tsconfig.app.json when updating this.
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      // "@components": fileURLToPath(new URL("./src/components", import.meta.url)), // Above will handle this automatically
+    },
+  },
 });
