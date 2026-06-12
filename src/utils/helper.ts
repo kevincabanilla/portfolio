a11b9327143d780a3efd8fe12add5cd0b2cc8f50
Kevin Cabanilla <43958285+kevincabanilla@users.noreply.github.com>
2026-06-12 19:01:50 +0800

    add getWebGLContext  and hasWebGL helper methods

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -5,3 +5,20 @@ export const scrollToId = (id: string) => {
   }
 };
 
+export const getWebGLContext = ():
+  | WebGLRenderingContext
+  | WebGL2RenderingContext
+  | null => {
+  if (typeof document === "undefined") return null;
+
+  const canvas = document.createElement("canvas");
+
+  return (canvas.getContext("webgl2") ||
+    canvas.getContext("webgl") ||
+    canvas.getContext("experimental-webgl")) as
+    | WebGLRenderingContext
+    | WebGL2RenderingContext
+    | null;
+};
+
+export const hasWebGL = (): boolean => !!getWebGLContext();
