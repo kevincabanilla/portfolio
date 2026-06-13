556a5b7ca3e106872dcaa35364027164125cb94a
Kevin Cabanilla <43958285+kevincabanilla@users.noreply.github.com>
2026-06-13 17:41:31 +0800

    create BaseProp model

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,3 +1,4 @@
+export * from "./base-props.interface";
 export * from "./navigation/nav-item.prop.interface";
 export * from "./navigation/nav-bar.prop.interface";
 export * from "./navigation/side-nav.prop.interface";
