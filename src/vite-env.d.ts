90a6953fee01d2f342accc5a0e600dcf3a38c163
Kevin Cabanilla <43958285+kevincabanilla@users.noreply.github.com>
2026-06-14 21:18:43 +0800

    Add environment variables

---
+++ src/vite-env.d.ts
@@ -0,0 +1,16 @@
+interface ViteTypeOptions {
+  // By adding this line, you can make the type of ImportMetaEnv strict
+  // to disallow unknown keys.
+  strictImportMetaEnv: unknown;
+}
+
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string;
+  readonly VITE_APP_DESCRIPTION: string;
+  readonly VITE_COUNTER_API_WORKSPACE: string;
+  readonly VITE_COUNTER_API_HANDLE: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
