cd032ce39a989ba74c2c469d1ccee1238a61bfa9
Kevin Cabanilla <43958285+kevincabanilla@users.noreply.github.com>
2026-06-14 12:03:40 +0800

    remove BaseProps

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,4 +1,3 @@
-export * from "./base-props.interface";
 export * from "./navigation/nav-item.prop.interface";
 export * from "./navigation/nav-bar.prop.interface";
 export * from "./navigation/side-nav.prop.interface";
