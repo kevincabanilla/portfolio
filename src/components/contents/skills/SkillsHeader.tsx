9e8d9b56f46f401b15bff2de958ed86ebd598ee5
Kevin Cabanilla <43958285+kevincabanilla@users.noreply.github.com>
2026-06-25 20:42:01 +0800

    Update SkillsHeader.tsx

--- src/components/contents/skills/SkillsHeader.tsx
+++ src/components/contents/skills/SkillsHeader.tsx
@@ -5,7 +5,7 @@ import { staggerItem } from "@/animations";
 export default function SkillsHeader({ children }: PropsWithChildren) {
   return (
     <motion.div variants={staggerItem}>
-      <div className="h-px bg-linear-to-r from-transparent via-cyan/60 to-transparent" />
+      <div className="h-px bg-linear-to-r from-transparent via-cyan/30 to-transparent" />
       <h3 className="text-lg md:text-2xl font-semibold text-primary text-center mt-6">
         {children}
       </h3>
